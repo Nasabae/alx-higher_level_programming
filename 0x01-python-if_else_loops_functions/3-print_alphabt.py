f54c3b590044d21eb2c53c34c659afbb6674e68d
Nasabae <ssakoto11@gmail.com>
2023-07-11 06:41:59 -0700

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for x in range(ord('a'), ord('z') + 1):
-      if chr(x) != 'e' and chr(x) != 'q':
-          print('{:c}'.format(x), end='')
+# Author - Serwah-Akoto Sandra
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
