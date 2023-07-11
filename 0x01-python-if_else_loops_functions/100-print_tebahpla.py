514757126d4462e873361da652e3f66f71dfd621
Nasabae <ssakoto11@gmail.com>
2023-07-11 07:25:11 -0700

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,5 +3,5 @@
 
 x = 0
 for c in range(ord('z'), ord('a') - 1, -1):
-print("{}".format(chr(c - x)), end="")
-x = 32 if x == 0 else 0
+    print("{}".format(chr(c - x)), end="")
+    x = 32 if x == 0 else 0
